aaccab7672845acfd27cf6b3f9f139e1a954ac0c
npiyush97 <npiyush35@gmail.com>
2023-12-07 12:35:49 +0530

    working ?

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+
+app.use(express.static(__dirname+'/frontend'))
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.sendFile(__dirname+'/index.html')
 })
 app.listen(process.env.PORT || 3000)
